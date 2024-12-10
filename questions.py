d50a7215cd754936469ac8820edf2c05c90c9613
simon-ww <41583701+simon-ww@users.noreply.github.com>
2024-12-10 17:12:00 -0500

    Update questions.py

--- questions.py
+++ questions.py
@@ -3,19 +3,19 @@ questions = {
         "I have a vivid imagination.",
         "I am full of ideas.",
         "I have a rich vocabulary.",
-        "I enjoy artistic and cultural experiences."
+        "I enjoy artistic and cultural experiences.",
         "I am always prepared.",
         "I pay attention to details.",
         "I get chores done right away.",
-        "I follow a schedule."
+        "I follow a schedule.",
         "I feel comfortable around people.",
         "I start conversations.",
         "I talk to a lot of different people at parties.",
-        "I am the life of the party."
+        "I am the life of the party.",
         "I sympathize with others' feelings.",
         "I take time out for others.",
         "I make people feel at ease.",
-        "I have a soft heart."
+        "I have a soft heart.",
         "I get stressed out easily.",
         "I worry about things.",
         "I am easily disturbed.",
